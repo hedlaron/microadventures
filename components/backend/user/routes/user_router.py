1292afe6c5097c9ffa6f16b55623ed189ba67298
Aron Hedl <hedlaron@example.com>
2025-07-04 19:47:49 +0200

    feat: Add current user dependency to user list and detail endpoints

--- components/backend/user/routes/user_router.py
+++ components/backend/user/routes/user_router.py
@@ -15,7 +15,7 @@ user_router = APIRouter(
 
 
 @user_router.get('/', response_model=list[UserSchema])
-def user_list(db: Session = Depends(get_db)):
+def user_list(db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     db_users = get_users(db)
 
     return db_users
@@ -28,7 +28,7 @@ def user_list(current_user: User = Depends(get_current_active_user)):
 
 
 @user_router.get('/{user_id}', response_model=UserSchema)
-def user_detail(user_id: int, db: Session = Depends(get_db)):
+def user_detail(user_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     db_user = get_user(db, user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
